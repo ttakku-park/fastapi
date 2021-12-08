040c3ee857349bc5cecd580f4035b28e5b7bcdd6
ttakku-park <ttakku.park@gmail.com>
2021-12-08 15:23:38 +0900

    feat:queryparameter

--- main.py
+++ main.py
@@ -1,17 +1,26 @@
 #import fastapi
+from typing import Optional
 from fastapi import FastAPI
+#request body를 설정하기 위해서는 pydantic 모델이 필요
+from pydantic import BaseModel
+import uvicorn
 
 #인스턴스 생성
 # uvicorn main:app --reload 의 app name
 app = FastAPI()
 
-@app.get('/') # / is baseurl , operation path
+#queryparameter
+#필수 매개변수 id:str / 기본값있는 매개변수 id:srt = '안녕' / 선택적선언 id : Optional[str] = None
+@app.get('/blog') # / is baseurl , operation path
 #handle the path
-def index():
-    return {'data':{'name':'jo'}}
-
+def index(limit=10, published:bool = True, q : Optional[str] = None):
+    if published :
+        return {'data':f'{limit} published blogs from the db,{q}'}
+    else:
+        return {'data':f'{limit} blogs from the db{q}'}
 #pathparameter
 #같은 경로 blog 를 사용하는경우 패스파라미터가 상단에 있으면 패스파라미터로 주소로 읽힘 
+#@app.get('/blog?limit=10&published=true')
 @app.get('/blog/unpublished')
 def unpublished():
     return {'data':'all unpublished blogs'}
@@ -21,6 +30,15 @@ def show(blog_id:int): #파라미터로 받을 타입설정
     return {'data':blog_id}
 
 @app.get('/blog/{blog_id}/comments')
-def comments(blog_id:int):
+def comments(blog_id):
     return {'data':{blog_id:'2'}}
+
+class Blog(BaseModel):
+    title : str
+    body : str
+    published_at : Optional[bool]
+
+@app.post('/blog')
+def create_blog(request: Blog):
+    return {'data':f'blog is created with {request.title}'}
 
