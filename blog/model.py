cd982f2276678f82b5363268899f6a43711e1fdf
ttakku-park <ttakku.park@gmail.com>
2021-12-10 14:41:48 +0900

    add:User Model 추가

--- blog/model.py
+++ blog/model.py
@@ -1,3 +1,4 @@
+from os import name
 from sqlalchemy import Boolean, Column, ForeignKey, Integer, String
 from .database import Base
 
@@ -6,7 +7,15 @@ from .database import Base
 #database 의 base
 class Blog(Base): 
     __tablename__ = 'blogs'
-    
+
     id = Column(Integer,primary_key=True, index=True)
     title = Column(String(50))
     body = Column(String(100))
+
+class User(Base):
+    __tablename__ = 'users'
+
+    id = Column(Integer,primary_key=True, index=True)
+    name = Column(String(50))
+    email = Column(String(100))
+    password = Column(String(500))
