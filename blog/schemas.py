b75f86c197fd29a43737655e94a108e508cb0f04
ttakku-park <ttakku.park@gmail.com>
2021-12-10 14:44:31 +0900

    add:응답을 위한 ShowUser 스키마 작성

--- blog/schemas.py
+++ blog/schemas.py
@@ -1,5 +1,5 @@
 from pydantic import BaseModel
-# pydantic model == schema
+# pydantic(response) model == schema / not related to database
 
 
 class Blog(BaseModel): #BaseModel 은 ID랑 같이 나오는듯?
@@ -10,6 +10,21 @@ class ShowBlog(Blog):
     
     title:str
     body: str
+    
     # db query사용, sqlalchemy의 orm을 사용하고있기 때문에 정의가 필요함
+    class Config():
+        orm_mode = True
+
+
+class User(BaseModel):
+    name : str
+    email : str
+    password : str
+
+
+class ShowUser(BaseModel):
+    name : str
+    email : str
+
     class Config():
         orm_mode = True
