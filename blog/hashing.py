6e26422ff86de35e058248ddc88c832ec9c7fe2d
ttakku-park <ttakku.park@gmail.com>
2021-12-10 14:43:48 +0900

    add:비밀번호 bcrypt암호화파일 생성

---
+++ blog/hashing.py
@@ -0,0 +1,9 @@
+from passlib.context import CryptContext
+
+pwd_cxt = CryptContext(schemes=['bcrypt'], deprecated='auto')
+
+class Hash():
+    def bcrypt(password:str):
+        hashedPassword = pwd_cxt.hash(password)
+
+        return hashedPassword
